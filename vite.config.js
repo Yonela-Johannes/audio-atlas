3b4dd836208f94afc928a1a24a419c8389c2b2ac
Yonela Johannes <johannesyonela@gmail.com>
2023-03-29 00:03:48 +0200

    chore: add base-url

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://Yonela-Johannes.github.io/audio-atlas/
 export default defineConfig({
-  server: {
-    host: true,
-  },
   plugins: [react()],
   base: './',
 });
